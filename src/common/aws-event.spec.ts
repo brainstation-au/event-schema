64c3dea5423e6a8d0191af799a0a56ab8340155e
Khalid Saifullah <khalid@outlook.com.au>
2022-07-06 08:28:30 +1000

    rearrange tests

--- src/common/aws-event.spec.ts
+++ src/common/aws-event.spec.ts
@@ -1,21 +1,23 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { z } from 'zod';
-import { UserAccountActivityDetail } from '.';
+import { AWSEvent } from './aws-event';
 
-describe('user account activity detail', () => {
+describe('aws event construct', () => {
+  const Detail = z.object({ foo: z.literal('bar') });
   test('parse a valid event document', () => {
-    const data = JSON.parse(readFileSync(join(__dirname, 'sample-input.json'), 'utf-8'));
-    expect(UserAccountActivityDetail.parse({
+    const data = JSON.parse(readFileSync(join(__dirname, 'aws-event-sample.json'), 'utf-8'));
+    expect(AWSEvent('random-source', 'some-type', Detail).parse({
       ...data,
       extra: 'bar',
     })).toEqual({
       ...data,
+      time: new Date(data.time),
     });
   });
 
   test('parse an invalid event document', () => {
-    expect(() => UserAccountActivityDetail.parse({
+    expect(() => AWSEvent('some-source', 'random-type', Detail).parse({
       jobname: 'foo',
       jobid: '4f4b5803-5885-4aa8-9c39-97fbf52a67b8',
       state: 'SUBMITTED',
--- src/red-team/service-b/job-state-change/index.spec.ts
+++ src/red-team/service-b/job-state-change/index.spec.ts
@@ -1,25 +1,24 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import { ZodError } from 'zod';
-import { JobStateChange } from '.';
+import { z } from 'zod';
+import { JobStateChangeDetail } from '.';
 
-describe('job state change', () => {
+describe('job state change detail', () => {
   test('parse a valid document', () => {
-    const data = JSON.parse(readFileSync(join(__dirname, 'sample-event.json'), 'utf-8'));
-    expect(JobStateChange.parse({
+    const data = JSON.parse(readFileSync(join(__dirname, 'sample-input.json'), 'utf-8'));
+    expect(JobStateChangeDetail.parse({
       ...data,
       extra: 'bar',
     })).toEqual({
       ...data,
-      time: new Date(data.time),
     });
   });
 
   test('parse an invalid document', () => {
-    expect(() => JobStateChange.parse({
+    expect(() => JobStateChangeDetail.parse({
       jobname: 'foo',
       jobid: '4f4b5803-5885-4aa8-9c39-97fbf52a67b8',
       state: 'SUBMITTED',
-    })).toThrow(ZodError);
+    })).toThrow(z.ZodError);
   });
 });
